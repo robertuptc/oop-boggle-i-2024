7e11c0ed067fcf8cde1b3dab3dbf3fe977a9db7b
robertuptc <robertuptc@gmail.com>
2024-07-02 06:37:58 -0400

    problem finished

--- boggle_board.py
+++ boggle_board.py
@@ -2,25 +2,48 @@ import random
 
 class BoggleBoard:
   def __init__(self):
+    self.boggle_board = [
+      "AAEEGN",
+      "ELRTTY",
+      "AOOTTW",
+      "ABBJOO",
+      "EHRTVW",
+      "CIMOTU",
+      "DISTTY",
+      "EIOSST",
+      "DELRVY",
+      "ACHOPS",
+      "HIMNQU",
+      "EEINSU",
+      "EEGHNW",
+      "AFFKPS",
+      "HLNNRZ",
+      "DEILRX"
+    ]
     print("----\n----\n----\n----\n")
 
 
   def shake(self):
-    board = ''
+    random_numbers_arr = self.get_random_nums(16, 4)
+    boggle_board = ''
 
-    for i in range(16):
-      for j in range(6):
-        random_letter = BoggleBoard.generate_random_letter()
-        if random_letter == "Qu":
-          board += random_letter + " "
-          random_letter = BoggleBoard.generate_random_letter()
+    for i in range(len(random_numbers_arr)):
+      random_numbers_arr_two = self.get_random_nums(6, 4)
+      list_of_letters = list(self.boggle_board[random_numbers_arr[i]])
+
+      for j in random_numbers_arr_two:
+        letter = list_of_letters[j]
+        
+        if letter == 'Q':
+          boggle_board += "Qu "
         else:
-          board += random_letter + "  "
-          random_letter = BoggleBoard.generate_random_letter()
-      board += "\n"
-    print(board)
+          boggle_board += f"{letter}  "
+      boggle_board += "\n"
+    return boggle_board
 
 
-  def generate_random_letter():
-    random_letter = (chr(random.randint(97, 121))).upper()
-    return random_letter if random_letter != 'Q' else 'Qu'
+  def get_random_nums(self, num, length):
+    input_nums = range(0,num)
+    random_numbers = random.sample(input_nums,length)
+    return random_numbers
+
